674c3670c2d4902706bc16bba4ed53aa3e49acf4
Hiroki Hayashi <hiroki.0220@gmail.com>
2022-03-02 21:46:19 +0900

    sql injection dummy

--- Program.cs
+++ Program.cs
@@ -1,10 +1,22 @@
 ﻿using System;
-
+using System.Data.SqlClient;
 public class Test
 {
 	public static void Main(string[] args)
 	{
+        SqlConnection conn = null;
 		Console.WriteLine(args[0]);
-	}
+        var sql = $"select * from datatable where id = '{args[0]}'";
+        using (SqlCommand command = new SqlCommand(sql, conn))
+        {
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine("{0} {1}", reader.GetString(0), reader.GetString(1));
+                }
+            }
+        }   
+    }
 }
 //comment
